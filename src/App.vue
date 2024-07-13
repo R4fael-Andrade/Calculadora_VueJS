e350ce8039847301349bb2ebea431b081d8f6ffa
Rafael <rafael.sousadrd@gmail.com>
2024-07-13 18:39:29 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { reactive, watch } from 'vue';
+import  Corpo from './components/Corpo.vue';
+  const estado = reactive ({
+    numero1: 0,
+    numero2: 0,
+    resultado: 0,
+    operacoes: 'somar',
+  })
+
+  const escolheOperacao = () => {
+    const {operacoes} = estado; 
+
+    switch (operacoes) {
+      case 'somar':
+          estado.resultado = parseFloat(estado.numero1) + parseFloat(estado.numero2);
+          break;
+      case 'subtrair':
+          estado.resultado = parseFloat(estado.numero1) - parseFloat(estado.numero2);
+          break;
+      case 'multiplicar':
+          estado.resultado = parseFloat(estado.numero1) * parseFloat(estado.numero2);
+          break;
+      case 'dividir':
+          estado.resultado = parseFloat(estado.numero1) / parseFloat(estado.numero2);
+          break;
+      default: 
+          estado.resultado  = 0;
+      }
+      return estado.resultado;
+      
+  }
+
+  watch(
+    () => [estado.numero1, estado.numero2, estado.operacoes],
+    escolheOperacao
+  )
+
+</script>
+
+<template>
+    <div class="container bg-light">
+      <Corpo :tipo-operacao="evento => estado.operacoes = evento.target.value" :valorn2="evento => estado.numero2 = evento.target.value" :valorn1="evento => estado.numero1 = evento.target.value" :get-operacao="escolheOperacao()" />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Corpo.vue
@@ -0,0 +1,55 @@
+<script setup>
+    const props =  defineProps(['getOperacao', 'valorn1', 'valorn2', 'tipoOperacao'])
+</script>
+
+<template>
+    <div class="row">
+        <h1 class="text-center">Calculadora <span>VueJS</span></h1>
+      </div>
+      <div class="row p-3">
+        <div class="col-md-4">
+          <input @keyup="props.valorn1" type="number" class="form-control">
+        </div>
+        <div class="col-md-4">
+          <input  @keyup="props.valorn2"  type="number" class="form-control">
+        </div>
+        <div class="col-md-3">
+          <select @change="props.tipoOperacao" class="form-control">
+            <option value="somar">Somar</option>
+            <option value="subtrair">Subtrair</option>
+            <option value="multiplicar">Multiplicar</option>
+            <option value="dividir">Dividir</option>
+          </select>
+        </div>
+      </div>
+      <div class="row mt-5">
+       <p class="fs-5" >
+        Resultado: {{ props.getOperacao }}
+       </p>
+      </div>
+</template>
+
+<style>
+    .container {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+    }
+
+    span {
+    color: #00cc00;
+    }
+
+    input[type=number]::-webkit-inner-spin-button,
+    input[type=number]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+
+.negativo {
+    color: red;
+}
+</style>
